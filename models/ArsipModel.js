a76d52bdb70cd0a582f4fe97910dd57bca5b7bc4
Irvan Na'syakban <irfanna.syakban098@gmail.com>
2024-01-24 01:32:01 +0700

    add Login

--- models/ArsipModel.js
+++ models/ArsipModel.js
@@ -1,5 +1,5 @@
 import { Sequelize } from "sequelize";
-import db from "../config/Database.js";
+import db from "../config/database.js";
 import Data from "./DataModel.js";
 
 const {DataTypes} = Sequelize;
